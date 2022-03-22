0ec6a6fc0a6ec650032e0ecbebb9a0f5c9943408
Ana Lívia <pereiraanalivia77@gmail.com>
2022-03-22 10:00:40 -0300

    Custom ButtonIcon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,7 +14,9 @@ const Home = () => {
                     <h1>Conheça o melhor catálogo de produtos</h1>
                     <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
                     </div>
+                    <div>
                     <Link to="/products"><ButtonIcon/></Link>
+                    </div>
                     
                 </div>
                 <div className="home-image-container">
